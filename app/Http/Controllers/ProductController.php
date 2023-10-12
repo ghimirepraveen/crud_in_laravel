19cc7ba0787761e6f6b1aee3cb072032098edac4
ghimirepraveen <ghimireprabin7777@gmail.com>
2023-10-12 21:39:21 +0545

    adding model

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -1,13 +1,27 @@
 <?php
 
 namespace App\Http\Controllers;
-
+use App\Models\product; 
 use Illuminate\Http\Request;
 
-class ProductController extends Controller
+class  ProductController extends Controller
 {
     public  function index(){
-
-        
+       return view('products.products'); 
     }
+    public  function create(){
+        return view('products.create'); 
+     }
+     public function store(Request $request){
+     
+         $data=$request->validate([
+            'name'=>'required',
+            'price'=>'required|numeric',
+            'qty'=>'required'
+            
+
+         ]);
+       $newproduct =product::create($data);
+       return redirect(route('product.index'));
+     }
 }
--- routes/web.php
+++ routes/web.php
@@ -14,11 +14,14 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
 
-Route::get('/product',[ProductController::class,'index']->name('product.index');
-);
+
+Route::get('/product',[ProductController::class,'index'])->name('product.index'); 
+
+Route::get('/product/create',[ProductController::class,'create'])->name('product.create'); 
+Route::post('/product',[ProductController::class,'store'])->name('product.store'); 
 
 
